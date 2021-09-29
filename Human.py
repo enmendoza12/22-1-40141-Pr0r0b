e8df50821dfafba7e89e985fffecc36388c2301b
NEFERTARI JALOMO <NEFERTARI_JALOMO@jabil.com>
2021-09-28 20:32:13 -0500

    00_first_class

---
+++ Human.py
@@ -0,0 +1,19 @@
+class Human:
+
+    born_year = "1996"
+
+    def say_hello(self):
+        print ("Hello")
+    
+    def say_hello_user(self, user_name):
+        print("Hello "+ user_name)
+
+human_one = Human()
+print(human_one)
+print(human_one.born_year)
+human_one.say_hello()
+human_one.say_hello_user("Nefertari")
+
+class Clothes
+
+    manufacture_date ="2021"
