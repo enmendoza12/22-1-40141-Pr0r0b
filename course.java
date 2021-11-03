b03fe0239e301ace26e516f1906b613e8e230ca5
enmendoza12 <enmendoza12@gmail.com>
2021-11-02 20:25:13 -0600

    Exam 2

---
+++ course.java
@@ -0,0 +1,16 @@
+public class Course {
+    String name;
+    String hour;
+    String teacher;
+    int classroom;
+
+
+    public Course (String name,String hour,String teacher,int classroom){
+    this.name = name;
+    this.hour = hour;
+    this.teacher = teacher;
+    this.classroom = classroom;
+    }
+    
+
+}
---
+++ main.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+	public static void Main(String[] args) {
+	    Course c1 = new Course ("OPP", "19:00-20:40", "Luis Guerra",5202);
+		
+
+        System.out.println("Nombre de la materia: "+ c1.name);
+        System.out.println("Horario: "+ c1.hour);
+        System.out.println("Profesor: "+ c1.teacher);
+        System.out.println("Salon: "+ c1.classroom);
+
+}
