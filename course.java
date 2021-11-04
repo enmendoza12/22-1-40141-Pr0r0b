742ab5d050a4bceb2a3f5e8945d17be7406c7030
enmendoza12 <enmendoza12@gmail.com>
2021-11-03 18:14:42 -0600

    Exam2

--- course.java
+++ course.java
@@ -1,4 +1,4 @@
-public class Course {
+class Course {
     String name;
     String hour;
     String teacher;
@@ -11,6 +11,24 @@ public class Course {
     this.teacher = teacher;
     this.classroom = classroom;
     }
+
+    public String unavailable_classroom (String name, classroom) {
+        return "Aula: "+name+"  Enseñando: "+subject;
+    }
+
+    public static void show_info(String name,String hour,String teacher,int classroom){
+        System.out.println("Nombre de la materia: "+name);
+        System.out.println("Horario: "+hour);
+        System.out.println("Profesor: "+teacher);
+        System.out.println("Salon: "+classroom);
+    }
     
+    public void welcome_message(){
+        System.out.println("Bienvenido al sistema de materias");
+    }
+
+    public int classroom_reserved(int classroom){
+        return "Aula: "+classroom;
+    }
 
 }
--- main.java
+++ main.java
@@ -1,13 +1,16 @@
 
-public class Main
-{
-	public static void Main(String[] args) {
+public class Main {
+	
+    public static void Main(String[] args) {
 	    Course c1 = new Course ("OPP", "19:00-20:40", "Luis Guerra",5202);
+        Course c2 = new Course ("Bases de Datos", "20:40-10:10", "Silvia Barrera",3205);
 		
+        welcome_message();
+        System.out.println("Materias dadas de alta: ");
+        c1.show_info();
+        c2.show_info();
+        classroom_reserved();
 
-        System.out.println("Nombre de la materia: "+ c1.name);
-        System.out.println("Horario: "+ c1.hour);
-        System.out.println("Profesor: "+ c1.teacher);
-        System.out.println("Salon: "+ c1.classroom);
+    }
 
 }
